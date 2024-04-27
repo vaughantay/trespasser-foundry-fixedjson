0b71a6eb90ee2cb58cf4f6f233ac1cb5b2e162d8
ShadowyXP <maxschreifels32@gmail.com>
2024-04-27 14:53:15 -0500

    Added data module for adventurer, with ground work for enemies as well.

--- module/trespasser.mjs
+++ module/trespasser.mjs
@@ -1,1 +1,12 @@
 import { TRESPASSER } from './helpers/config.mjs';
+import { AdventurerData } from './module/data_models/adventurer.mjs';
+
+Hooks.once('init', function () {
+
+	game.trespasser = {
+		
+	};
+	
+	CONFIG.Actor.dataModels.adventurer = AdventurerData;
+
+});
