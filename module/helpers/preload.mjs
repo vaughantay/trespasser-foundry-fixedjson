f9a03265ae96e16f06bad45cd7385e74a09924ce
cuttl-fish <olivia.botha.0@gmail.com>
2024-05-02 16:29:34 +0200

    fixed my oopsie

--- module/helpers/preload.mjs
+++ module/helpers/preload.mjs
@@ -1,6 +1,10 @@
 export const loadHandlebarsPartials = () => {
   const partials = [
 		'systems/trespasser/templates/actor-adventurer-sheet.hbs',
+    "systems/trespasser/templates/actor/adventurer/skills-tab.hbs",
+		"systems/trespasser/templates/actor/adventurer/equip-tab.hbs",
+		"systems/trespasser/templates/actor/adventurer/combat-tab.hbs",
+    "systems/trespasser/templates/actor/adventurer/equipment-slot.hbs"
   ]
 
   return loadTemplates(partials)
--- module/trespasser.mjs
+++ module/trespasser.mjs
@@ -4,16 +4,6 @@ import { TrespasserActorSheet } from './sheets/actor-sheet.mjs';
 import { AdventurerData } from './data_models/adventurer.mjs';
 import { loadHandlebarsPartials } from './helpers/preload.mjs'
 
-async function preloadHandlebarTemplates(){
-	const templatePaths = [
-		"systems/trespasser/templates/actor/adventurer/skills-tab.hbs",
-		"systems/trespasser/templates/actor/adventurer/equip-tab.hbs",
-		"systems/trespasser/templates/actor/adventurer/combat-tab.hbs"
-	];
-	return loadTemplates(templatePaths);
-};
-
-
 Hooks.once('init', function () {
 
 	game.trespasser = {
