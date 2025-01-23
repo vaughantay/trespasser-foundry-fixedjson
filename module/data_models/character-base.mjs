dccd40a60b3ec34a0a2f006f8a56a4ea764a62e2
liv <168260028+cuttl-fish@users.noreply.github.com>
2025-01-23 12:32:27 +0200

    sheet updating & attribute moving

--- module/data_models/character-base.mjs
+++ module/data_models/character-base.mjs
@@ -3,36 +3,6 @@ export class CharacterBaseData extends foundry.abstract.TypeDataModel {
 
 		const fields = foundry.data.fields;
 		return {
-			attributes: new fields.SchemaField({
-				might: new fields.NumberField({
-					required: true,
-					initial: 1,
-					integer: true,
-					min: 0,
-					max: 10
-				}),
-				agility: new fields.NumberField({
-					required: true,
-					initial: 1,
-					integer: true,
-					min: 0,
-					max: 10
-				}),
-				intellect: new fields.NumberField({
-					required: true,
-					initial: 1,
-					integer: true,
-					min: 0,
-					max: 10
-				}),
-				spirit: new fields.NumberField({
-					required: true,
-					initial: 1,
-					integer: true,
-					min: 0,
-					max: 10
-				})
-			}),
 			combat: new fields.SchemaField({
 				hit_points: new fields.SchemaField({
 					//Pending question on how to make the max of this equal to max. May need to handle in change listener, which is fine.
